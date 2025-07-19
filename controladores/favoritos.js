3b301c5046ac6f41984dc961755ad4c5f1cd2be2
Walber Amorim <walber_amorim@hotmail.com>
2025-07-19 09:46:17 -0300

    fix: arrumando crud favoritos

--- controladores/favoritos.js
+++ controladores/favoritos.js
@@ -5,48 +5,40 @@ import {
     deletarFavorito 
 } from '../servicos/favoritos.js';
 
-function getFavoritos(req, res) {
+async function getFavoritos(req, res) {
     try {
-        const favoritos = getTodosOsFavoritos();
+        const favoritos = await getTodosOsFavoritos();
         res.send(favoritos);
     } catch (error) {
         res.status(500).send(error);
     }
 }
 
-function getFavorito(req, res) {
+async function getFavorito(req, res) {
     try {
-        const id = Number(req.params.id);
-        if (!id || Number.isNaN(id)) {
-            return res.status(422).send('Id do favorito deve ser um número válido');
-        }
-        const favorito = getFavoritoPorId(id);
+        const id = req.params.id;
+        const favorito = await getFavoritoPorId(id);
         res.send(favorito);
     } catch (error) {
         res.status(500).send(error);
     }
 }
 
-function postFavorito(req, res) {
-    const id = Number(req.params.id);
+async function postFavorito(req, res) {
     try {
-        if (!req.body.nome) {
-            return res.status(422).send('O nome do favorito deve ser informado');
-        }
-        salvarFavorito(req.body);
+        const id = req.params.id;
+        console.log('id: ',id);
+        await salvarFavorito(id);
         res.send('Favorito salvo com sucesso!');
     } catch (error) {
         res.status(500).send(error);
     }
 }
 
-function deleteFavorito(req, res) {
+async function deleteFavorito(req, res) {
     try {
-        const id = Number(req.params.id);
-        if (!id || Number.isNaN(id)) {
-            return res.status(422).send('Id do favorito deve ser um número válido');
-        }
-        deletarFavorito(id);
+        const id = req.params.id;
+        await deletarFavorito(id);
         res.send('Favorito deletado com sucesso!');
     } catch (error) {
         res.status(500).send(error);
--- servicos/favoritos.js
+++ servicos/favoritos.js
@@ -1,4 +1,5 @@
 import Favoritos from '../models/Favoritos.js';
+import Livros from '../models/Livros.js';
 
 async function getTodosOsFavoritos() {
     return await Favoritos.find({});
@@ -8,9 +9,24 @@ async function getFavoritoPorId(id) {
     return await Favoritos.findById(id);
 }
 
-async function salvarFavorito(favorito) {
+async function salvarFavorito(id) {
     try {
-        await Favoritos.create(favorito);
+        const favExiste = await Favoritos.findById(id);
+        if (favExiste) {
+            throw new Error('Livro já favoritado');
+        }
+        const favorito = await Livros.findById(id);
+        if (!favorito) {
+            throw new Error('Livro não encontrado');
+        }
+        const novoFav = {
+            _id: favorito._id,
+            nome: favorito.nome,
+            editora: favorito.editora,
+            preco: favorito.preco,
+            paginas: favorito.paginas
+        }
+        await Favoritos.create(novoFav);
     } catch (error) {
         console.log(error);
     }
